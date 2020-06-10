1c406ebb3f466b4850896e848430bd7593956e7a
joydeep1701 <joydeep1701@gmail.com>
2020-06-10 20:06:40 +0530

    Adds Bootstrap to project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3100;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('pages/index', {
