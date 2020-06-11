2d9b9236dab2a3d244a6198fb876eec2612b84bc
joydeep1701 <joydeep1701@gmail.com>
2020-06-11 15:48:26 +0530

    Adds City & State finder APIs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const geoRouter = require('./routes/geo');
+
 const app = express();
 const PORT = process.env.PORT || 3100;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static('static'))
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('pages/index', {
@@ -13,6 +17,12 @@ app.get('/', (req, res) => {
   });
 });
 
+// app.get('/report', (req, res) => {
+//   res.render('pages/report', {
+//   });
+// });
+
+app.use('/geo/', geoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Started on PORT ${PORT}`);
