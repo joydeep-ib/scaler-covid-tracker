db2680ba65c68350926693f0a6ab35a3f7e3919e
joydeep1701 <joydeep1701@gmail.com>
2020-06-11 18:53:17 +0530

    Adds static serving & routers

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const geoRouter = require('./routes/geo');
+const reportsRouter = require('./routes/report');
 
 const app = express();
 const PORT = process.env.PORT || 3100;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use('/static', express.static('static'))
 app.use(express.json());
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
   res.render('pages/index', {
@@ -17,12 +20,8 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.get('/report', (req, res) => {
-//   res.render('pages/report', {
-//   });
-// });
-
 app.use('/geo/', geoRouter);
+app.use('/reports/', reportsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Started on PORT ${PORT}`);
